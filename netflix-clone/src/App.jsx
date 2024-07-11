6bb1c857a6978902028dca6d92ed316a95077de7
Kerubal Wake <kerubalwake@Kerubals-MacBook-Pro.local>
2024-07-11 12:01:33 -0500

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,10 +1,15 @@
+import './App.css'
+import Home from './Pages/Home/Home'
+
+
 
 function App() {
    return (
-     <div className="App">
-       <h1>Hello</h1>
-     </div>
-   );
+    <div className='App'>
+      <Home/>
+    </div>
+   )
+   
 }
 
 export default App
